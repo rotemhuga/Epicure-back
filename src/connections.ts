153990df48a25a62b0d0d3d99dc95af912a88884
rotem huga <rotem.3007@gmail.com>
2023-03-19 17:15:50 +0200

    update the env

--- src/connections.ts
+++ src/connections.ts
@@ -1,12 +1,12 @@
 import {connect} from "mongoose"
-const uri = "mongodb+srv://RotemHuga:Rot249853@cluster0.ppyhnan.mongodb.net/EpicureDB"
+// const uri = "mongodb+srv://RotemHuga:Rot249853@cluster0.ppyhnan.mongodb.net/EpicureDB"
 import dotenv from "dotenv" 
 dotenv.config()
-// const mongoUri: any = process.env.TOKEN_SECRET 
+const mongoUri:any = process.env.TOKEN_SECRET;
 
 export const connectToDB = async () => {
 try {
-    await connect(uri);
+    await connect(mongoUri);
     console.log('db connected')
   } catch (err) {
     console.log("error connecting to db", err);
