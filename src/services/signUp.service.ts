cb8136023e54c8bd0fffcce5eef4e519047e7699
rotem huga <rotem.3007@gmail.com>
2023-03-20 19:29:14 +0200

    working on sign up  - 20.03

--- src/services/signUp.service.ts
+++ src/services/signUp.service.ts
@@ -1,9 +1,38 @@
 import {signUpModel} from "../model/signUp.model"
-import { ISignUpBack } from "../model/signUp.model";
+import {ISignUpBack} from "../model/signUp.model";
+import bcrypt from "bcrypt";
+
+// export const getSignUp = async (user: any) => {
+//     const { firstName, lastName, emailAddress, shippingAddress, password} = user
+  
+//     const encryptedPassword = await bcrypt.hash(password, 10)
+  
+//     const newUser = new UserModel({
+//       firstName,
+//       lastName,
+//       email: emailAddress.toLowerCase(),
+//       shippingAddress,
+//       password: encryptedPassword,
+//     })
+  
+//     try {
+//       const oldUser = await UserModel.findOne({ emailAddress })
+  
+//       if (oldUser) {
+//         throw new Error('User already exists')
+//       }
+  
+//       const savedUser = await newUser.save()
+//       return savedUser
+  
+//     } catch (e) {
+//       throw new Error('Error while creating new user')
+//     }
+//   }
 
 export const getSignUp = async (user: ISignUpBack) => {
-try {
-    console.log("users services connected");
+
+    try {
     const _userSign = await signUpModel.create(user)
     await _userSign.save();
         return _userSign
