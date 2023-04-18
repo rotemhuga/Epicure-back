511b82294f5ebe230d02e1b0e72944877d03fc04
rotem huga <rotem.3007@gmail.com>
2023-04-18 11:58:42 +0300

    adding middleware - 18.04

--- src/routes/chefs.routes.ts
+++ src/routes/chefs.routes.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from "express";
 import {getAllChefs, deleteClickedChef} from "../controllers/chefs.controllers"
 
 const router = express.Router();
+const middleWare = require('../../src/middleWare/isAdmin');
 
 router.get('/ChefsPage', getAllChefs);
 // router.delete('/ChefsPage', deleteClickedChef);
